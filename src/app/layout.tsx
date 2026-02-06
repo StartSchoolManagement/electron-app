47a05c97cec5fcd9d89fc8a8224cfb2ec8ad77f1
Eliass Baranovs <eliass.baranovs@gmail.com>
2026-02-06 16:34:14 +0200

    Added google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Script from 'next/script'
 import AppLifecycle from '@/components/AppLifecycle'
 import ErrorBoundary from '@/components/ErrorBoundary'
 
@@ -14,6 +15,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FV8D69PVFV"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FV8D69PVFV');
+          `}
+        </Script>
+      </head>
       <body>
         <AppLifecycle />
         <ErrorBoundary>
